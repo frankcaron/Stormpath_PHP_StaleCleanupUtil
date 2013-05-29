ec0c17ea42ab13764fc82e236090d7b7c4702a7b
Frank Caron <frank@frankcaron.com>
2013-05-29 18:16:35 -0400

    Reintroduced Stormpath SDK
    Reintroduced the Stormpath SDK but to no effect just yet. :(

--- Stormpath_CleanUpUtil.php
+++ Stormpath_CleanUpUtil.php
@@ -15,29 +15,31 @@
 error_reporting(-1);
 
 //Import Stormpath lib
-//require 'stormpath-sdk-php/Services/Stormpath.php';
-include('httpful.phar');
+require 'stormpath-sdk-php/Services/Stormpath.php';
+
+//Import credentials file
+require_once 'Stormpath_Credentials.php';
 
 //Connection vars
-
 $appHref = "https://" . urlencode($appID) . ":" . urlencode($appSecret) . "@api.stormpath.com/v1/applications/" . $appUID;
-
-//Mission go
-echo "Alright, let's do this, monkey.<br /><br />";
 
 //Connect
 try{  
-	$uri = $appHref . "/accounts";
 	
-	echo "Gimme " . $uri;
-	x
-	$response = Request::get($uri)->send();
+	//Mission go
+	echo "Alright, let's do this, Apollo.<br /><br />";
+
+	$builder = new Services_Stormpath_Client_ClientApplicationBuilder;
+	$clientApplication = $builder->setApplicationHref($appHref)->build();
 	
-	echo "yup";
+	$client = $clientApplication->getClient();
+	$application = $clientApplication->getApplication();
+	
+	echo "Apollo, we have no problem.<br /><br />";
 	
 } catch (Exception $e) {
 	//Log error
-	echo "Error encountered: " . $e;
+	echo "Apollo, we had a problem:<br /><br /> " . $e;
 }
 
 
